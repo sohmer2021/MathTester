293c1b60d50689f46bdfb99238a939a0fba86e7d
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-02-07 07:41:31 -0500

    Created intellij

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,31 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                // write your code here
+                String firstWord;
+                String secondWord;
+                int firstNumber, secondNumber, total;
+
+                firstWord ="dog";
+                secondWord ="house";
+                firstNumber =5;
+                secondNumber =10;
+                total = firstNumber+secondNumber;
+                System.out.println(total);
+                total = total + 10;
+                System.out.println(total);
+
+
+        /*System.out.println(firstWord + secondWord);
+        System.out.println(firstWord + (firstNumber + secondNumber));
+        System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+        System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
+        System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
+        System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("the modulus of the secondNumber by the firstNumber"+
+                " is "+ (secondNumber%firstNumber));*/
+    }
+}
